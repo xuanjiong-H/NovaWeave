5390ddbb646f77c1a4a3813d556a309be73acddf
xuanjiong-H <bdhc_huang@163.com>
2026-04-25 16:46:31 +0800

    feat:多数据源和mapper配置

---
+++ ai-agent-station-study-infrastructure/src/main/java/cn/moonice/infrastructure/dao/po/AiAgentFlowConfig.java
@@ -0,0 +1,46 @@
+package cn.moonice.infrastructure.dao.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * 智能体-客户端关联表
+ * @author bugstack虫洞栈
+ * @description 智能体-客户端关联表 PO 对象
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AiAgentFlowConfig {
+
+    /**
+     * 主键ID
+     */
+    private Long id;
+
+    /**
+     * 智能体ID
+     */
+    private Long agentId;
+
+    /**
+     * 客户端ID
+     */
+    private Long clientId;
+
+    /**
+     * 序列号(执行顺序)
+     */
+    private Integer sequence;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+}
