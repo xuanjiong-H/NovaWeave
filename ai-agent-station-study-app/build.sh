7e3cde640ad928b5f00a92f9867637490da7ef77
xuanjiong-H <bdhc_huang@163.com>
2026-07-24 14:18:19 +0800

    update:版本更新v2.3

--- ai-agent-station-study-app/build.sh
+++ ai-agent-station-study-app/build.sh
@@ -1,5 +1,5 @@
 # 普通镜像构建，随系统版本构建 amd/arm
-docker build --load -t moonice/ai-agent-station-study-app:2.2 -f ./Dockerfile .
+docker build --load -t moonice/ai-agent-station-study-app:2.3 -f ./Dockerfile .
 
 # 兼容 amd、arm 构建镜像
 # docker buildx build --l oad --platform liunx/amd64,linux/arm64 -t xiaofuge/xfg-frame-archetype-app:1.0 -f ./Dockerfile . --push
